2a4f417916c1635694f97a847562660befbd304b
Andrew M <fileformat@gmail.com>
2012-09-25 23:14:52 -0400

    fixed urldecoding problem with spaces

--- app.js
+++ app.js
@@ -142,11 +142,11 @@ function serveTest(query, response)
 			{
 				if (kv[0] in params)
 				{
-					params[kv[0]].push(querystring.unescape(kv[1]));
+					params[kv[0]].push(querystring.unescape(kv[1].replace(/\+/g, " ")));
 				}
 				else
 				{
-					params[kv[0]] = [ querystring.unescape(kv[1]) ];
+					params[kv[0]] = [ querystring.unescape(kv[1].replace(/\+/g, " ")) ];
 				}
 			}
 			else
